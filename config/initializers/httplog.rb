e0e6b3c358715c3700aa12a527bece972c74081d
Brian Sam-Bodden <bsbodden@integrallis.com>
2016-03-03 10:49:21 -0700

    Changed httplog level to INFO

--- config/initializers/httplog.rb
+++ config/initializers/httplog.rb
@@ -1,2 +1,3 @@
 HttpLog.options[:logger] = Rails.logger
-HttpLog.options[:color] = {color: :black, background: :light_red}  
+HttpLog.options[:color] = {color: :black, background: :light_red}
+HttpLog.options[:severity] = Logger::Severity::INFO
